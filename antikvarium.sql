dd51c18b112da885800368ce1a7dce7bd7d38c01
borbasb47 <borbasb47@gmail.com>
2024-11-27 17:30:59 +0100

    Add files via upload

--- antikvarium.sql
+++ antikvarium.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Nov 25. 13:54
--- Kiszolgáló verziója: 10.4.32-MariaDB
--- PHP verzió: 8.2.12
+-- Létrehozás ideje: 2024. Nov 27. 17:30
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -60,10 +60,6 @@ CREATE TABLE `felhasznalo` (
   `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
---
--- A tábla adatainak kiíratása `felhasznalo`
---
-
 -- --------------------------------------------------------
 
 --
@@ -101,6 +97,19 @@ CREATE TABLE `konyvek` (
 -- --------------------------------------------------------
 
 --
+-- Tábla szerkezet ehhez a táblához `kosar`
+--
+
+CREATE TABLE `kosar` (
+  `mennyiseg` int(11) NOT NULL,
+  `ar` int(11) NOT NULL,
+  `felhasznaloId` int(11) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Tábla szerkezet ehhez a táblához `megrendeles`
 --
 
@@ -110,7 +119,8 @@ CREATE TABLE `megrendeles` (
   `telefonszam` text NOT NULL,
   `iranyitoszam` int(11) NOT NULL,
   `utca` text NOT NULL,
-  `hazszam` text NOT NULL
+  `hazszam` text NOT NULL,
+  `felhasznaloId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 -- --------------------------------------------------------
@@ -122,6 +132,29 @@ CREATE TABLE `megrendeles` (
 CREATE TABLE `mufajok` (
   `id` int(11) NOT NULL,
   `megnevezes` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `termekek`
+--
+
+CREATE TABLE `termekek` (
+  `cim` text NOT NULL,
+  `alkoto` text NOT NULL,
+  `forgalmazo` text NOT NULL,
+  `id` int(11) NOT NULL,
+  `ar` int(11) NOT NULL,
+  `kiadas` text NOT NULL,
+  `kategoria` text NOT NULL,
+  `mufaj` text NOT NULL,
+  `borito` text NOT NULL,
+  `nyelv` text NOT NULL,
+  `hossz` text NOT NULL,
+  `osszefoglalo` text NOT NULL,
+  `allapot` text NOT NULL,
+  `tipus` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
@@ -153,10 +186,18 @@ ALTER TABLE `konyvek`
   ADD PRIMARY KEY (`arukod`);
 
 --
+-- A tábla indexei `kosar`
+--
+ALTER TABLE `kosar`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `felhasznaloId` (`felhasznaloId`);
+
+--
 -- A tábla indexei `megrendeles`
 --
 ALTER TABLE `megrendeles`
-  ADD PRIMARY KEY (`rendelesszam`);
+  ADD PRIMARY KEY (`rendelesszam`),
+  ADD KEY `felhasznaloId` (`felhasznaloId`);
 
 --
 -- A tábla indexei `mufajok`
@@ -165,34 +206,78 @@ ALTER TABLE `mufajok`
   ADD PRIMARY KEY (`id`);
 
 --
+-- A tábla indexei `termekek`
+--
+ALTER TABLE `termekek`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- A kiírt táblák AUTO_INCREMENT értéke
 --
+
+--
+-- AUTO_INCREMENT a táblához `cd`
+--
+ALTER TABLE `cd`
+  MODIFY `arukod` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT a táblához `felhasznalo`
 --
 ALTER TABLE `felhasznalo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
-ALTER TABLE `cd`
-  MODIFY `arukod` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+--
+-- AUTO_INCREMENT a táblához `kategoriak`
+--
 ALTER TABLE `kategoriak`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
+--
+-- AUTO_INCREMENT a táblához `konyvek`
+--
 ALTER TABLE `konyvek`
   MODIFY `arukod` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
-ALTER TABLE `mufajok`
+--
+-- AUTO_INCREMENT a táblához `kosar`
+--
+ALTER TABLE `kosar`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
+--
+-- AUTO_INCREMENT a táblához `megrendeles`
+--
 ALTER TABLE `megrendeles`
   MODIFY `rendelesszam` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `mufajok`
+--
+ALTER TABLE `mufajok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `termekek`
+--
+ALTER TABLE `termekek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `kosar`
+--
+ALTER TABLE `kosar`
+  ADD CONSTRAINT `kosar_ibfk_1` FOREIGN KEY (`felhasznaloId`) REFERENCES `felhasznalo` (`id`);
+
+--
+-- Megkötések a táblához `megrendeles`
+--
+ALTER TABLE `megrendeles`
+  ADD CONSTRAINT `megrendeles_ibfk_1` FOREIGN KEY (`felhasznaloId`) REFERENCES `felhasznalo` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
