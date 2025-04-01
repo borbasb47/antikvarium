39da3e393f96e1a36995a8bd3a86ba7bd3cd7ead
borbasb47 <borbasb47@gmail.com>
2025-04-01 14:31:52 +0200

    Add files via upload

--- 1017projekt/public/javascripts/termekAdat.js
+++ 1017projekt/public/javascripts/termekAdat.js
@@ -1,10 +1,37 @@
 function displayBookData(){
-    var productId = getQueryVariable("id");
+    const queryString = window.location.search;
+    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    const productId = urlParams.get('id')
+    console.log(productId);
+   
     fetch(`/1017projekt/api/termek.php?id=${productId}`)   
     .then(response => response.json())
     .then(adat => {
         console.log(adat)
         console.log(productId)
+        document.getElementById("cim").innerHTML+=adat.cim
+        document.getElementById("leiras").innerHTML+=adat.osszefoglalo
+        document.getElementById("szerzo").innerHTML+=adat.alkoto
+        document.getElementById("kiado").innerHTML+=adat.forgalmazo
+        document.getElementById("megjelenes").innerHTML+=adat.megjelenes
+        document.getElementById("allapot").innerHTML+=adat.allapot
+        document.getElementById("nyelv").innerHTML+=adat.nyelv
+        const xd=document.getElementById("hossz").innerHTML+=adat.hossz
+        document.getElementById("borito").innerHTML+=adat.borito
+        document.getElementById("meret").innerHTML+=adat.meret
+        document.getElementById("ar").innerHTML+=adat.ar
+
+        if(adat.tipus=="könvy"){
+            xd.innerHTML+=" oldal"
+        }
+        else{
+            xd.innerHTML+=" perc"
+        }
+
+
+        document.createElement("productPicture").src=adat.kepURL
+        
     })
     .catch(error => console.error('Hiba xd:', error));
 
